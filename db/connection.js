ba3e77cc62997d8244d7f5344ad7630a18117bde
DrBubblez <harrisonminer59@gmail.comgit config --global uder.name DrBubblezgit config --global user.email harrisonminer59@gmail.com>
2023-09-21 01:47:06 -0400

    Added Main Menu() & viewEmployees()

--- db/connection.js
+++ db/connection.js
@@ -1,21 +1,27 @@
+// Importing Dependencies
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+// Declaring global variable
+let db;
+
+// Function to connect to database
 async function connectDB() {
   try {
-    const dbConfig = {
-        host: '127.0.0.1',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+    const dbConfig = { // Database configuration
+        host: '127.0.0.1', 
+        user: process.env.DB_USER, // Your username imported from .env
+        password: process.env.DB_PASSWORD, // Your password imported from .env
+        database: process.env.DB_NAME // Your database name imported from .env
     };
     
-    const connection = await mysql.createConnection(dbConfig);
+    // Create a new connection using the config details
+    db = await mysql.createConnection(dbConfig);
     console.log("Connected to the database.");
-    return connection;
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 }
 
-module.exports = { connectDB };
+// Exporting the connection and a function to get the database
+module.exports = { connectDB, getDb: () => db };
--- server.js
+++ server.js
@@ -1,16 +1,83 @@
+// Importing dependencies
 const express = require('express');
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
 
+// Setting up port and express app
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const { connectDB, db } = require('./db/connection');
+// Importing database connection
+const { connectDB, getDb } = require('./db/connection');
 
+// Connect to database
 connectDB();
 
+// Main Menu Function
+const mainMenu = async () => {
+    const { choice } = await inquirer.prompt([ // Prompt user for choice
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: [
+                'View Employees',
+                'View Departments',
+                'View Roles',
+                'Add Employee',
+                'Add Department',
+                'Add Role',
+                'Update Employee Role',
+                'Exit'
+            ]
+        }
+    ])
+    switch (choice) { // Switch statement to handle user choice
+        case 'View Employees':
+            viewEmployees();
+            break;
+        case 'View Departments':
+            viewDepartments();
+            break;
+        case 'View Roles':
+            viewRoles();
+            break;
+        case 'Add Employee':
+            addEmployee();
+            break;
+        case 'Add Department':
+            addDepartment();
+            break;
+        case 'Add Role':
+            addRole();
+            break;
+        case 'Update Employee Role':
+            updateEmployeeRole();
+            break;
+        case 'Exit':
+            db.end();
+            break;
+    }
+}
+
+// View Employees Function
+const viewEmployees = async () => {
+    try { // Try to query database for all employees
+      const [employees] = await getDb().query('SELECT * FROM employee');
+      console.table(employees);
+    } catch (error) { // Catch and display any errors
+      console.error('Error fetching employees:', error);
+    }
+    mainMenu(); // Return to main menu
+};
+
+mainMenu();
+
+// Listen for server connection
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
